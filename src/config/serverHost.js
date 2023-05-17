a126191a6046bd0fb8ad8c13ad91f80bb1f30704
KsHuyZ <92804746+KsHuyZ@users.noreply.github.com>
2023-05-17 22:16:09 +0700

    new

--- src/config/serverHost.js
+++ src/config/serverHost.js
@@ -1,6 +1,6 @@
 export const serverHost = {
   local: "http://localhost:4000",
-  product: "https://calendar-backend-nine.vercel.app",
+  product: "https://calendarbe.onrender.com",
 };
 export const mapApiKey = "oC8CNdh20xrH8Dpm0SIkZYQqBijW847QWVmBE0DB";
 export const weatherKey = "8b4a1cfe7b37f251dcce8b232975fd6d";
